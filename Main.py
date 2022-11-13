cda56f5dd9b5a9744c27cca54c649299b4f5dcf6
DavidJong05 <36602654+DavidJong05@users.noreply.github.com>
2022-11-13 11:53:29 +0100

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,5 +1,3 @@
-from typing import List, Tuple, Union, Dict
-import sys
 from Lexer import *
 from Parser import *
 from AST_classes import *
@@ -8,11 +6,12 @@ sys.setrecursionlimit(200000)
 
 
 def main():
-    prog3 = "\n40 G = 8, Z = 9 \n52 IF G = 8 PRINT 'Its 7' \n60 G = G + 1, N = 8 \n70 IF G=N GOTO 52 \n80 IF G>N PRINT 'LOWER'\n90 IF G<N PRINT 'HIGHER'\n100 A = 2\n110 PRINT 'YOU GUESSED IT', A = 1"
-    prog5 = "\n40 IF 10 = 11 PRINT 'knap hoor', P = 12, Y = 7\n60 Z = 11, P = 33\n80 PRINT 'janee'"
-    prog6 = "\n40 G = 10, A = 10\n60 G = G + 1\n70 GOTO 60\n110 PRINT 'done', A = 1"
-    prog7 = "\n40 G = 12, Z = 10 \n41 G = G + Z \n50 IF G = 100 GOTO 80 \n60 G = G + 1\n70 GOTO 50\n80 PRINT 'ITS 100 NOW!'"
-    lexed = lexer(prog7, [])
+    #f = open("basic", "r")
+    f = open("basic2", "r")
+    #f = open("basic3", "r")
+    s = f.read()
+    s = '\n' + s[0:]
+    lexed = lexer(s, [])
     p1 = Parser(lexed)
     file = p1.parseProgram()
     interp = Interpreter(file)
